33185821a3187dcd91cc96c72139241765d380d5
VGeorgiev <vgeorgiev@up2technology.com>
2023-01-18 17:00:13 +0200

    Configure the product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+import { urlFor } from "lib/client";
+import Link from "next/link";
 import React from "react";
 import { ProductType } from "types";
 
@@ -7,7 +10,25 @@ interface ProductProps {
 }
 
 const Product: React.FC<ProductProps> = ({ product }) => {
-  return <div>Product</div>;
+  const { slug, name, price, image } = product;
+
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0]).url()}
+            width={250}
+            height={250}
+            alt={name}
+            className="product-image"
+          />
+          <p className="product.name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
